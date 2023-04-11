0678e59f304cbe80fdce0355d0c2f548db33982d
Jyoti Rajpal <114987227+Rjyoti11webvillee@users.noreply.github.com>
2023-04-11 22:56:49 +0530

    Update my.router.js

--- router/my.router.js
+++ router/my.router.js
@@ -7,6 +7,7 @@ router.get('/testing1', testcontroller.testing1);
 router.get('/testing2', testcontroller.testing2);
 router.get('/testing3', testcontroller.testing3);
 router.get('/testing4', testcontroller.testing4);
+router.get('/testing5', testcontroller.testing5);
 
 
 
