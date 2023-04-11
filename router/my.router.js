a17ccb83d71a69352840fa59dcac6c4dffda7964
Jyoti Rajpal <114987227+Rjyoti11webvillee@users.noreply.github.com>
2023-04-11 21:24:48 +0530

    Update my.router.js

--- router/my.router.js
+++ router/my.router.js
@@ -4,5 +4,6 @@ var testcontroller = require('../controller/test.controller');
 router.get('/greetings', testcontroller.greetings);
 router.get('/testing', testcontroller.testing);
 router.get('/testing1', testcontroller.testing1);
+router.get('/testing2', testcontroller.testing2);
 
 module.exports = router;
